eb29a45b61a2177e2fc8cd315ed5b26fe93d0136
DaniilLugivsky <propro.organav@gmail.com>
2024-05-02 17:18:39 +0300

    lesson1 lesson2

--- src/lesson2/QA27_2_7_Lugovskiy.java
+++ src/lesson2/QA27_2_7_Lugovskiy.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class QA27_2_7_Lugovskiy {
     public static void main(String[] args){
-        System.out.print("Введите колличество числел ,последовательности Фибоначи: ");
-        Scanner scanner = new Scanner(System.in);
-        int koll = scanner.nextInt();
-        int n0 = 1;
-        int n1 = 1;
-        int n2;
-        System.out.print(n0+" "+n1+" ");
-        for(int i = 3; i <= koll; i++){
-            n2=n0+n1;
-            System.out.print(n2+" ");
-            n0=n1;
-            n1=n2;
+        while (true) {
+            System.out.print("Введите колличество числел ,последовательности Фибоначи: ");
+            Scanner scanner = new Scanner(System.in);
+            int koll = scanner.nextInt();
+            int n0 = 1;
+            int n1 = 1;
+            int n2;
+            System.out.print(n0 + " " + n1 + " ");
+            for (int i = 3; i <= koll; i++) {
+                n2 = n0 + n1;
+                System.out.print(n2 + " ");
+                n0 = n1;
+                n1 = n2;
+            }
+            System.out.println(" - Данные числа");
         }
-        System.out.println(" - Данные числа");
-        scanner.close();
     }
 }
